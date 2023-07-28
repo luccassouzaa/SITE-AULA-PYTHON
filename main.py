431627b9873639b08e688d3651251d258fc79611
Luccas <luccasmelim@yahoo.com.br>
2023-07-28 02:52:59 -0300

    Att 1.0 (id's, import's and socketio.run())

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 @app.route("/")
 def homepage():
     return render_template("index.html")
 
-app.run()
+SocketIO.run(app, host="localhost")
