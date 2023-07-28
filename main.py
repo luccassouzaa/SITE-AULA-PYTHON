3cd4bbc3dd991838b49cca6c0fc9dc3f65afe722
Luccas <luccasmelim@yahoo.com.br>
2023-07-28 02:56:43 -0300

    att 2.0

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ from flask_socketio import SocketIO, send
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
+#funcionalidade de enviar mensagem
+@socketio.on("message")
+def sendMessage(message):
+    send(message, broadcast=True)
+
 @app.route("/")
 def homepage():
     return render_template("index.html")
